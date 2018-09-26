c488988e3ac7449b75413413358f94b7ab34a88e
™ധู้ざຣ้ণს✚ປิʨℓℓҨබମ™ <39564544+0954517662@users.noreply.github.com>
2018-09-26 15:13:53 +0700

    Update py3.py

--- py3.py
+++ py3.py
@@ -1,18 +1,13 @@
 # -*- coding: utf-8 -*-
-
-from LINEPY import *
-from akad.ttypes import *
-from multiprocessing import Pool, Process
+from linepy import *
+from akad.ttypes import Message
 from datetime import datetime
+from time import sleep
 from bs4 import BeautifulSoup
+from humanfriendly import format_timespan, format_size, format_number, format_length
+import time, random, sys, json, codecs, threading, glob, re, string, os, requests, subprocess, six, ast, pytz, urllib, urllib.parse
 from gtts import gTTS
 from googletrans import Translator
-import time,random,sys,json,codecs,threading,glob,re,os,subprocess
-from datetime import datetime, timedelta
-from humanfriendly import format_timespan, format_size, format_number, format_length
-import requests
-import datetime
-import requests,urllib,json
 
 cl = LINE()
 #cl = LINE('Tokenmu Sayang')
